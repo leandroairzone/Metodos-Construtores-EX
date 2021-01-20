d9dacdc9b328533755a293c0093b4e8f4258abab
unknown <leandroiay3@gmail.com>
2021-01-20 12:53:31 -0300

    Metodo Contrutor EX

---
+++ classes/Produto.cs
@@ -0,0 +1,26 @@
+namespace MetodoS_coNstrutores_EX.classes
+{
+    public class Produto
+    {
+        
+        public int Codigo{get; set;}
+        public string Nome{get; set;}
+        public string Descricao{get; set;}
+        public int Estoque{get; set;}
+
+        public Produto() {
+            
+        }
+
+        public Produto(int codigo) {
+            Codigo = codigo;
+        }
+
+        public Produto(int codigo, string nome, string descricao, int estoque) {
+            Codigo = codigo;
+            Nome = nome;
+            Descricao = descricao;
+            Estoque = estoque;
+        }
+    }
+}
